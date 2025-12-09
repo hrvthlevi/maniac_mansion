dd06c95d973113e5b37715651b010bb1547efa08
hrvthleventeroland <horvathleventeroland@gmail.com>
2025-12-09 11:44:14 +0100

    fogyaszthato, kulcstargyak modositas

--- jatek/targyak/kulcs_targyak.py
+++ jatek/targyak/kulcs_targyak.py
@@ -1,6 +1,4 @@
-from jatek.targyak import Kulcs_targyak
-
-level = Kulcs_targyak("Levél",False)
-kulcs = Kulcs_targyak("Kulcs",False)
-kotel = Kulcs_targyak("Kötél",False)
-lampa = Kulcs_targyak("Lámpa",False)
+level = False
+kulcs = False
+kotel = False
+lampa = False
