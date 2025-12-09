935f54d98a0be8bc11a7bae9b68b8f18ad018733
HuszárFruzsinaAnna <fruzsinaanna.huszar@gmail.com>
2025-12-09 10:47:53 +0100

    osszekotes kezdetlegesen

--- jatek/jatek_logika/jatekmenet.py
+++ jatek/jatek_logika/jatekmenet.py
@@ -1,26 +1,29 @@
-from jatek.illusztraciok.hatter import postalada, borton_1, bejarat
+import jatek.helyszinek as hely
+from jatek.targyak import kulcs_targyak as kulcs
+import jatek.jatek_logika.esetek as esetek
 
-eset1()
-if bejarat and kulcs:
-    eset2()
-elif bejarat:
-        eset3()
+def jatekmenet():
+    esetek.eset1()
+    if hely.bejarat and kulcs.kulcs:
+        esetek.eset2()
+    elif hely.bejarat:
+            esetek.eset3()
 
-if postalada:
-    eset4()
+    if hely.postalada:
+        esetek.eset4()
 
-if konyha and level:
-    eset5()
-elif konyha:
-    eset6()
+    if hely.konyha and kulcs.level:
+        esetek.eset5()
+    elif hely.konyha:
+        esetek.eset6()
 
-if borton:
-    eset7()
+    if hely.borton:
+        esetek.eset7()
 
-if lepcso:
-    eset8()
-elif konyha and sult_hus:
-    eset9()
+    if hely.lepcso:
+        esetek.eset8()
+    elif hely.konyha and kulcs.sult_hus:
+        esetek.eset9()
 
 
 
