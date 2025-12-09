40d81ee6e5d779ca165473c3452d7866cd3e9f60
HuszárFruzsinaAnna <fruzsinaanna.huszar@gmail.com>
2025-12-09 10:24:31 +0100

    esemenykezelo kezdetleges

---
+++ jatek/beallitas/tarol.py
@@ -0,0 +1,1 @@
+jatekos_nev = ""
---
+++ jatek/jatek_logika/esetek.py
@@ -0,0 +1,16 @@
+from jatek.beallitas.tarol import jatekos_nev
+
+WIDTH = 120
+
+def eset1():
+    print("{:^120}".format("Elrabolták a legjobb barátodat. Készen állsz megmenteni?"))
+
+    jatekos = input("Hogy szólíthatunk? ")
+
+    szoveg = (f"Ez egy magánterület, {jatekos}. Biztos vagy a belépésben?\n"
+              f"Vállalod, hogy kísérletezzenek rajtad?")
+
+    for sor in szoveg.split("\n"):
+        print("{:^120}".format(sor))
+
+eset1()
