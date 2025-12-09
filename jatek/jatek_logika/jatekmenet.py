4e19376beafa5e69dfaeed00ae46f22a7a18d670
MGergo20 <medgyaszayg3@gmail.com>
2025-12-09 10:31:10 +0100

    hatter.py, jatekmenet.py

---
+++ jatek/jatek_logika/jatekmenet.py
@@ -0,0 +1,28 @@
+from jatek.illusztraciok.hatter import postalada, borton_1, bejarat
+
+eset1()
+if bejarat and kulcs:
+    eset2()
+elif bejarat:
+        eset3()
+
+if postalada:
+    eset4()
+
+if konyha and level:
+    eset5()
+elif konyha:
+    eset6()
+
+if borton:
+    eset7()
+
+if lepcso:
+    eset8()
+elif konyha and sult_hus:
+    eset9()
+
+
+
+
+
